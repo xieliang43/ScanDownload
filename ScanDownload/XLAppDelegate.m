6deae6a7963dcb49756ed2af850f00a8b86ebb8a
Albert <xieliang43@126.com>
2012-06-14 21:03:03 +0800

    adsfsdf

--- ScanDownload/XLAppDelegate.m
+++ ScanDownload/XLAppDelegate.m
@@ -26,6 +26,9 @@
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
+    
+    application.statusBarStyle = UIStatusBarStyleBlackTranslucent;
+    
     self.viewController = [[[XLViewController alloc] initWithNibName:@"XLViewController" bundle:nil] autorelease];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
