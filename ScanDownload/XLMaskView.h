3f435bf9320b74392aa50d9e872e8534cb35b497
xieliang <xieliang43@126.com>
2012-06-15 00:05:09 +0800

    add some file

---
+++ ScanDownload/XLMaskView.h
@@ -0,0 +1,13 @@
+//
+//  XLMaskView.h
+//  ScanDownload
+//
+//  Created by xie liang on 12-6-15.
+//  Copyright (c) 2012年 pretang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XLMaskView : UIView
+
+@end
---
+++ ScanDownload/XLMaskView.m
@@ -0,0 +1,31 @@
+//
+//  XLMaskView.m
+//  ScanDownload
+//
+//  Created by xie liang on 12-6-15.
+//  Copyright (c) 2012年 pretang. All rights reserved.
+//
+
+#import "XLMaskView.h"
+
+@implementation XLMaskView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
